01ec283c0b2b487f004c7f98312dbae7cdcd1ac1
devil0px <devil0px@gmail.com>
2022-06-12 23:26:53 +0200

    working on thy bolg

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from turtle import title
+from unittest import mock
+from django.db import models
+
+# Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=10000)
+    
