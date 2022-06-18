7ece3256b9c418682b2e3ae8232d727eda95ea06
devil0px <devil0px@gmail.com>
2022-06-18 20:48:39 +0200

    add blog models

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,18 @@
+from csv import list_dialects
+from pyexpat import model
 from django.contrib import admin
 
 # Register your models here.
-from  .models import post
-admin.site.register(post)
+from  .models import post ,Category
+
+class postAdmin(admin.ModelAdmin):
+    list_dialects=['id','title','category']
+    search_fields= ['title','content']
+    #date_hierarchy ='publish_date'
+
+
+
+admin.site.register(post,postAdmin)
+admin.site.register(Category)
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,38 @@
+from pyexpat import model
+from tabnanny import verbose
 from turtle import title
 from unittest import mock
 from django.db import models
+from django.forms import ImageField
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) :
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural="categories"
+        
+
+
+
+
 
 # Create your models here.
 class post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=10000)
-    
+    #publish_date = models.DateField(default=timezone.now)
+    #update_date = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='posts/')
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self) :
+        return self.title
+
+    class Meta:
+       verbose_name= "my posts"
+       #ٍordering = ['-publish_date']
